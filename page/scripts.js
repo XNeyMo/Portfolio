bd7ab6110765e26db9ecf3091bcd7d0c6746103d
Neyan Montes <neyanmontes611@gmail.com>
2023-06-19 12:55:30 -0500

    Nav Sticky and Sections Template

--- page/scripts.js
+++ page/scripts.js
@@ -4,3 +4,23 @@ var typed = new Typed(".home-span-input", {
     backSpeed: 60,
     loop: true
 });
+
+var  mn = $(".main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('header').height();
+    sec = $('section');
+    secs = "section-scrolled";
+
+var scrollThreshold = (window.innerHeight * 1) - (window.innerHeight * 0.08) - 50;
+
+$(window).scroll(function() {
+    if ( $(this).scrollTop() > scrollThreshold ) {
+        mn.addClass(mns);
+        sec.addClass(secs);
+    } 
+        
+    else {
+        mn.removeClass(mns);
+        sec.removeClass(secs);
+    }
+});
