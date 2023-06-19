1f01f9ea86ea926c7c58eebbc2c4b5a54c8d0210
Neyan Montes <neyanmontes611@gmail.com>
2023-06-19 11:46:12 -0500

    Header V1

---
+++ page/scripts.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".home-span-input", {
+    strings:["Neyan Montes.", "Systems Engineer.", "Mechatronic Engineer.", "Front End Developer."],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop: true
+});
