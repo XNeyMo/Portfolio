7f0a0c1c8e0917bf08fd74772ad6d51e32b0e3c6
XNeyMo <xneymodev@gmail.com>
2023-09-18 10:10:43 -0500

    Netlify and Github

---
+++ assets/scripts.js
@@ -0,0 +1,21 @@
+const typed = new Typed(".phrases", {
+    strings: ["Web Developer", "Software Developer"],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop: true,
+})
+
+function openKnowledge(knowledge) {
+    var sections = document.getElementsByClassName("knowledge");
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = "none";
+    }
+
+    var buttons = document.getElementsByTagName("button");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("active");
+    }
+
+    document.getElementById(knowledge).style.display = "grid";
+    document.querySelector(`button[onclick="openKnowledge('${knowledge}')"]`).classList.add("active");
+}
