5d81b86b74acc36a30533663a59efca4d9158578
XNeyMo <xneymodev@gmail.com>
2023-09-20 11:59:24 -0500

    About finished

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,3 +4,18 @@ const typed = new Typed(".phrases", {
     backSpeed: 60,
     loop: true,
 })
+
+function openSkills(items) {
+    var sections = document.getElementsByClassName("skills-items");
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = "none";
+    }
+
+    var buttons = document.getElementsByTagName("button");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("button-active");
+    }
+
+    document.getElementById(items).style.display = "grid";
+    document.querySelector(`button[onclick="openSkills('${items}')"]`).classList.add("button-active");
+}
