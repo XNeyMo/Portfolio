292afa2cdc15cf9bd3afc67f16f33ec4e137aa30
XNeyMo <xneymodev@gmail.com>
2023-09-21 01:06:31 -0500

    Netlify finished

--- netlify/scripts.js
+++ netlify/scripts.js
@@ -5,17 +5,17 @@ const typed = new Typed(".phrases", {
     loop: true,
 })
 
-function openKnowledge(knowledge) {
-    var sections = document.getElementsByClassName("knowledge");
+function openSkills(items) {
+    var sections = document.getElementsByClassName("skills-items");
     for (var i = 0; i < sections.length; i++) {
         sections[i].style.display = "none";
     }
 
     var buttons = document.getElementsByTagName("button");
     for (var i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove("active");
+        buttons[i].classList.remove("button-active");
     }
 
-    document.getElementById(knowledge).style.display = "grid";
-    document.querySelector(`button[onclick="openKnowledge('${knowledge}')"]`).classList.add("active");
+    document.getElementById(items).style.display = "grid";
+    document.querySelector(`button[onclick="openSkills('${items}')"]`).classList.add("button-active");
 }
